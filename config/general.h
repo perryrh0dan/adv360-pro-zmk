bd8b759de99d06ea2b9965e4cbf3736fd2a9b8b0
perryrh0dan <thomas@tpoe.dev>
2024-08-27 08:34:32 +0000

    feat(right): switch up and down

--- config/general.h
+++ config/general.h
@@ -14,12 +14,3 @@
     tapping-term-ms = <200>;
     quick-tap-ms = <QUICK_TAP_MS>;
 };
-
-// long-tap action on hold, short-tap action on all interrupts
-/*#define MT_CORE \*/
-/*    flavor = "tap-preferred"; \*/
-/*    tapping-term-ms = <220>; \*/
-/*    quick-tap-ms = <220>; \*/
-/*    hold-trigger-key-positions = <0>;*/
-/**/
-/*&mt { MT_CORE };*/
